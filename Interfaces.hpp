83664472e04975ed9cb70d6db09be908fe0b91c8
Khanh <khanhpham102006@gmail.com>
2025-11-13 19:59:49 -0500

    .

--- Interfaces.hpp
+++ Interfaces.hpp
@@ -1,72 +1,42 @@
 #pragma once
 
-#include "Interfaces.hpp"
-#include "LinkedList.hpp"
+#include <cstddef>
 #include <stdexcept>
 
 template <typename T>
-class LLDeque : public DequeInterface<T> {
-private:
-    LinkedList<T> list;
+class StackInterface {
 public:
-    // Constructor
-    LLDeque() = default;
+    virtual ~StackInterface() = default;
 
-    // Insertion
-    void pushFront(const T& item) override {
-        list.addHead(item);
-    }
+    virtual void push(const T& item) = 0;
+    virtual T pop() = 0;
+    virtual const T& peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
+};
 
-    void pushBack(const T& item) override {
-        list.addTail(item);
-    }
 
-    // Deletion
-    T popFront() override {
-        if (list.getCount() == 0) {
-            throw std::runtime_error("Deque is empty");
-        }
-        T val = list.getHead()->data;
-        list.removeHead();
-        return val;
-    }
+template <typename T>
+class QueueInterface {
+public:
+    virtual ~QueueInterface() = default;
 
-    T popBack() override {
-        if (list.getCount() == 0) {
-            throw std::runtime_error("Deque is empty");
-        }
-        T val = list.getTail()->data;
-        list.removeTail();
-        return val;
-    }
+    virtual void push(const T& item) = 0;
+    virtual T pop() = 0;
+    virtual const T& peek() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
+};
 
-    // Access
-    const T& front() const override {
-        auto head = list.getHead();
-        if (!head) {
-            throw std::runtime_error("Deque is empty");
-        }
-        return head->data;
-    }
 
-    const T& back() const override {
-        auto tail = list.getTail();
-        if (!tail) {
-            throw std::runtime_error("Deque is empty");
-        }
-        return tail->data;
-    }
+template <typename T>
+class DequeInterface {
+public:
+    virtual ~DequeInterface() = default;
 
-    // Getter
-    std::size_t getSize() const noexcept override {
-        return list.getCount();
-    }
-
-    void printForward() const {
-        list.printForward();
-    }
-
-    void printReverse() const {
-        list.printReverse();
-    }
+    virtual void pushFront(const T& item) = 0;
+    virtual void pushBack(const T& item) = 0;
+    virtual T popFront() = 0;
+    virtual T popBack() = 0;
+    virtual const T& front() const = 0;
+    virtual const T& back() const = 0;
+    virtual std::size_t getSize() const noexcept = 0;
 };
